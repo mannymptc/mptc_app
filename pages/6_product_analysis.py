f3bbfca4a14dd19a63cbfbe0b6e8322c78ea8721
mannymptc <mantavyamptc@gmail.com>
2025-05-29 10:13:41 +0100

    6_product_analysis.py

--- pages/6_product_analysis.py
+++ pages/6_product_analysis.py
@@ -177,11 +177,11 @@ with tab1:
     past_df['SKU'] = past_df['product_sku']
     past_df['Name'] = past_df['product_name']
 
-    def make_history_matrix(df, value_col):
+    def def make_history_matrix(df, value_col):
         pivot = df.pivot_table(index=['Month', 'Week'], columns='SKU', values=value_col, aggfunc='sum')
         pivot = pivot.sort_index(ascending=False).fillna("-")
         sku_names = df[['SKU', 'Name']].drop_duplicates().set_index('SKU')['Name'].to_dict()
-        pivot.columns = pd.MultiIndex.from_tuples([(sku, sku_names.get(sku, '')) for sku in pivot.columns])
+        pivot.columns = [f"{sku} | {sku_names.get(sku, '')}" for sku in pivot.columns]
         return pivot
 
     qty_matrix = make_history_matrix(past_df, 'product_qty')
